4f382752c0677774dd5d7bc5f8c787045303be0e
Rmarcial <rogerpires@gmail.com>
2020-05-03 23:39:31 +0100

    Bubble Sort implementation to sort a list

---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+"""
+ Implementation of Bubble sort algorithm
+"""
+
+
+def bubble_sort(lst):
+    """
+    Bubble sort: sorts a list
+    O(n²)
+    :param lst:
+    :return: sorted list
+    """
+
